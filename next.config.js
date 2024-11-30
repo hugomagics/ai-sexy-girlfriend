e8b6f995fc84690f4bd0367f67f24462e9df000f
magics <hugo1.rodrigues@epitech.eu>
2024-11-30 23:27:13 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async redirects() {
+    return [
+      {
+        source: '/visit/candy-ai',
+        destination: 'https://candy.ai/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
