8367a3c387ef99293d591fe6642d11b3ce3591a5
magics <hugo1.rodrigues@epitech.eu>
2024-11-30 23:17:37 +0100

    fix

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -6,7 +6,7 @@ import HeroButton from "./HeroButton";
 export default function Hero() {
   return (
     <section className="relative mt-12 flex items-center justify-center bg-[#151820]">
-      <div className="absolute inset-0 bg-[url('/images/ai-girlfriend-banner.jpg')] bg-cover bg-center opacity-10"></div>
+      <div className="absolute inset-0 bg-cover bg-center opacity-10"></div>
       <div className="container mx-auto px-4 text-center relative z-10">
         <div className="flex flex-row w-fit mx-auto items-center mb-6 gap-10">
           <HeroTitle />
